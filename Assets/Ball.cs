d659ffac7cce22bb593f32ea56ced138e550bf94
Stuart Bowman <stuartsoft2012@gmail.com>
2017-04-22 15:52:36 -0400

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public bool ballHasBeenThrown
+    {
+        get; private set;
+    }
+
+    private Rigidbody rb;
+
+    public void grabBall()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void throwBall()
+    {
+        ballHasBeenThrown = true;
+    }
+
+    public void resetBall()
+    {
+        ballHasBeenThrown = false;
+        rb.isKinematic = true;
+
+        transform.position = new Vector3(0, 2.0f, 0);
+        ballHasBeenThrown = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        ballHasBeenThrown = false;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (ballHasBeenThrown && rb.velocity.magnitude < 0.1f)
+        //{
+        //    rb.velocity = Vector3.zero;
+        //    rb.angularVelocity = Vector3.zero;
+
+        //    transform.position = new Vector3(0, 0.5f, 0);
+        //    ballHasBeenThrown = false;
+        //}
+    }
+
+    
+
+}
---
+++ Assets/Scripts/ControllerCollider.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCollider : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // 2
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log("grip");
+            GameObject.Find("Sphere").GetComponent<Ball>().resetBall();
+        }
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+
+        objectInHand.GetComponent<Ball>().grabBall();
+
+        FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
+        fixedJoint.breakForce = 99999;
+        fixedJoint.breakTorque = 99999;
+
+        fixedJoint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<FixedJoint>())
+        {
+            // 2
+            FixedJoint fixedJoint = GetComponent<FixedJoint>();
+            fixedJoint.connectedBody = null;
+            Destroy(fixedJoint);
+            // 3
+
+            objectInHand.GetComponent<Ball>().throwBall();
+
+            Rigidbody rb = objectInHand.GetComponent<Rigidbody>();
+            rb.velocity = Controller.velocity;
+            rb.angularVelocity = Controller.angularVelocity;
+
+            //buff
+            rb.AddForce(rb.velocity.normalized*1000);
+        }
+        // 4
+        objectInHand = null;
+    }
+
+}
